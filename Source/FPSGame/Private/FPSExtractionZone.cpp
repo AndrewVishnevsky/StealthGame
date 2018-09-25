c6cd634efb09e475e29bd11a0bf7c3363a16c8d7
Andrew Vishnevsky <vishnev93@gmail.com>
2018-09-25 23:53:23 +0900

    Minor Fix

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -24,6 +24,6 @@ AFPSExtractionZone::AFPSExtractionZone()
 
 void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Overlapped with extraction zone!"));
+	UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone!"));
 }
 
--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -22,7 +22,7 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, Category="Components")
 	UBoxComponent* OverlapComp;
-
+	UFUNCTION()
 	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
